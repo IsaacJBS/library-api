09f36960cb8dc7c93f5f75c310015379b118d7ac
Isaac <isaac.jordaobs@gmail.com>
2022-07-25 16:41:09 -0300

    feat: subject entity

--- src/entities/Room.ts
+++ src/entities/Room.ts
@@ -1,4 +1,11 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm'
+import {
+  Column,
+  Entity,
+  ManyToMany,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm'
+import { Subject } from './Subject'
 import { Video } from './Video'
 
 @Entity('rooms')
@@ -11,4 +18,7 @@ export class Room {
 
   @OneToMany(() => Video, video => video.room)
   videos: Video[]
+
+  @ManyToMany(() => Subject, subject => subject.rooms)
+  subjects: Subject[]
 }
